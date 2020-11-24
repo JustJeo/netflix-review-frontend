079fa53e9d9eaa961f9c0f4a47cf25981db49463
jmcho311 <jmcho311@gmail.com>
2020-11-24 10:16:43 -0800

    updated Card component with img

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,6 +4,7 @@ const Card = (props) => {
   return (
     <div className="MovieCard">
       <h2>{ props.category }</h2>
+      <img src={ props.coverArtUrl } alt="Netflix image"/>
       <h3>{ props.title }</h3>
       <h5>By: { props.publisher }</h5>
       <p>{ props.content }</p>
