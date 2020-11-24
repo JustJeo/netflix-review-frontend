162c5f72a22ce0d96f45aaf230b65b93369d4ced
jmcho311 <jmcho311@gmail.com>
2020-11-23 22:56:34 -0800

    connected back to front, created review model, updated Card component to pull all db info, updated Home page to show all review info

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,33 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReviewModel from '../models/review'
+
 import Card from '../components/Card'
 
-const Home = () => {
-  return (
-    <container>
-      <div className="logo">
-        <h1>Netflix</h1>
-        <h3>-----TV Recaps-----</h3>
-      </div>
-      <div className="trending">
-        <h2>Trending</h2>
-        <Card />
-        <Card />
-        <Card />
-      </div>
-      <div className="lgbt">
-        <h2>LGBTQIA+</h2>
-        <Card />
-        <Card />
-        <Card />
-      </div>
-      <div className="voices">
-        <h2>Black Voices</h2>
-        <Card />
-        <Card />
-        <Card />
-      </div>
-    </container>
-  );
+
+class Home extends Component {
+  state = {
+    reviews: []
+  }
+
+  componentDidMount() {
+    this.fetchReviewData()
+  }
+
+  fetchReviewData = () => {
+    ReviewModel.all().then(data => {
+      this.setState({ reviews: data.reviews })
+    })
+  }
+
+  render() {
+    let reviewList = this.state.reviews.map((review, index) => {
+      return (
+        <div key={index}>
+          <Card {...review} />
+        </div>
+      )
+    })
+
+    return (
+      <container>
+        <div className="logo">
+          <h1>Netflix</h1>
+          <h3>-----TV Recaps-----</h3>
+        </div>
+        <div className="reviews">
+          { this.state.reviews ? reviewList : 'Loading...'}
+        </div>
+        {/* <div className="trending">
+          <h2>Trending</h2>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <div className="lgbt">
+          <h2>LGBTQIA+</h2>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <div className="voices">
+          <h2>Black Voices</h2>
+          <Card />
+          <Card />
+          <Card />
+        </div> */}
+      </container>
+    )
+  }
 }
 
 export default Home;
