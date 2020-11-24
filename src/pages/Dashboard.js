ebb158a616bc1bc299a22cab844cd8ed95a4792f
jmcho311 <jmcho311@gmail.com>
2020-11-24 12:50:22 -0800

    added total users and articles to dashboard page

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react';
 import UserModel from '../models/user'
+import ReviewModel from '../models/review'
 
 import UserCard from '../components/User'
 
 class Dashboard extends Component {
     state = {
-        users: []
+        users: [],
+        reviews: []
     }
 
     componentDidMount() {
         this.fetchUserData()
+        this.fetchReviewData()
     }
 
     fetchUserData = () => {
         UserModel.all().then(data => {
+            console.log(data.users.length)
             this.setState({ users: data.users })
         })
     }
     
+    fetchReviewData = () => {
+        ReviewModel.all().then(data => {
+            this.setState({ reviews: data.reviews })
+        })
+    }
+
     render() {
         let userList = this.state.users.map((user, index) => {
             return (
@@ -28,6 +38,8 @@ class Dashboard extends Component {
         })
         return (
             <div>
+                <div>{ this.state.users.length } Total Sign-Ups</div>
+                <div>{ this.state.reviews.length } Total Articles</div>
                 { this.state.users ? userList : 'Loading...'}
             </div>
         );
