a2df202ab15e0109c6b71b1574e95d6cc277f642
jmcho311 <jmcho311@gmail.com>
2020-11-24 12:26:21 -0800

    created link in footer to dashboard and testing modal

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Footer.scss'
 
 const Footer = () => {
@@ -10,7 +11,9 @@ const Footer = () => {
             <li>About us</li>
             <li>Subscribe</li>
             <li>Careers</li>
-            <li>Company Login</li>
+            <li>
+              <Link to={`/dashboard`}>Company Login</Link>
+            </li>
           </ul>
       </div>
       <div className = "categoriesList">
