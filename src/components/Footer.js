b6e2f634f958c0d093ebaeea7c09b596452be2f4
JustJeo <j.cabrigas.writer@gmail.com>
2020-11-23 19:05:28 -0800

    footer is set

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import './Footer.scss'
 
 const Footer = () => {
-  return <p>Footer Here!</p>
+  return (
+    <footer className = "footer">
+      <div className = "companyList">
+        <h4>Company</h4>
+          <ul>
+            <li>About us</li>
+            <li>Subscribe</li>
+            <li>Careers</li>
+            <li>Company Login</li>
+          </ul>
+      </div>
+      <div className = "categoriesList">
+        <h4>Categories</h4>
+          <ul>
+            <li>Romance</li>
+            <li>Comedy</li>
+            <li>Reality</li>
+            <li>Documentary</li>
+          </ul>
+      </div>
+      <div className = "email">
+        <h4>Want more? Join our weekly newsletter</h4>
+        <input type="email" placeholder="email address"></input>
+        <button>Go!</button>
+      </div>
+      <div className = "connect">
+        <h4>Connect</h4>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer;
